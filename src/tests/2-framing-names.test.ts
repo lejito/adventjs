4a67a00dfdc780abe6c39c037c67f9a8dc2127c3
Lejitop <alejocrrs@gmail.com>
2024-12-11 16:23:11 -0500

    Refactor test cases in challenge 2 to use newline characters for expected output

--- src/tests/2-framing-names.test.ts
+++ src/tests/2-framing-names.test.ts
@@ -4,26 +4,18 @@ import { createFrame } from "../challenges/2-framing-names";
 describe("Challenge 2: Framing Names", () => {
   test("Case 1", () => {
     const frame1 = createFrame(["midu", "madeval", "educalvolpz"]);
-    expect(frame1).toBe(`***************
-* midu        *
-* madeval     *
-* educalvolpz *
-***************`);
+    expect(frame1).toBe(
+      "***************\n* midu        *\n* madeval     *\n* educalvolpz *\n***************"
+    );
   });
 
   test("Case 2", () => {
     const frame2 = createFrame(["midu"]);
-    expect(frame2).toBe(`********
-* midu *
-********`);
+    expect(frame2).toBe("********\n* midu *\n********");
   });
 
   test("Case 3", () => {
     const frame3 = createFrame(["a", "bb", "ccc"]);
-    expect(frame3).toBe(`*******
-* a   *
-* bb  *
-* ccc *
-*******`);
+    expect(frame3).toBe("*******\n* a   *\n* bb  *\n* ccc *\n*******");
   });
 });
